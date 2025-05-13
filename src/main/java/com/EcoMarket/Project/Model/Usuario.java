cea8660427030db1274c04c44699b6287f786a8a
Markduoc <marc.corrales@duocuc.cl>
2025-05-13 11:54:30 -0400

    Dependencias terminadas
    se quitó y se agregó las dependencias necesarias

--- src/main/java/com/EcoMarket/Project/Model/Usuario.java
+++ src/main/java/com/EcoMarket/Project/Model/Usuario.java
@@ -1,5 +1,12 @@
 package com.EcoMarket.Project.Model;
 
+
 public class Usuario {
+    
+    private Integer id;
+    private String prNombre;
+    private String seNombre;
+    private String apPaterno;
+    private String apMaterno;
 
 }
