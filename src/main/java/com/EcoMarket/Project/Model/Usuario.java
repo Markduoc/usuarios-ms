5308df72566872739e50202f07072f72124eaa67
Markduoc <marc.corrales@duocuc.cl>
2025-05-13 12:17:08 -0400

    Creacion clase usuario
    Se crea la clase usuario y se añade a la base de datos

--- src/main/java/com/EcoMarket/Project/Model/Usuario.java
+++ src/main/java/com/EcoMarket/Project/Model/Usuario.java
@@ -1,12 +1,41 @@
 package com.EcoMarket.Project.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name= "Usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable=false, length=13, unique=true)
+    private Integer run;
+
+    @Column(nullable=false)
     private String prNombre;
+
+    @Column(nullable=false)
     private String seNombre;
+
+    @Column(nullable=false)
     private String apPaterno;
+
+    @Column(nullable=false)
     private String apMaterno;
+
+
 
 }
