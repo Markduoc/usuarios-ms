6fd441b2d72a720e9fbaaa38ae2e0652ca9f4d01
Markduoc <marc.corrales@duocuc.cl>
2025-05-17 17:42:44 -0400

    Se crean los metodos de Post

--- src/main/java/com/EcoMarket/Project/Controller/UsuarioController.java
+++ src/main/java/com/EcoMarket/Project/Controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.EcoMarket.Project.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,13 @@ import com.EcoMarket.Project.Model.Usuario;
 import com.EcoMarket.Project.Repository.UsuarioRepository;
 import com.EcoMarket.Project.Service.UsuarioService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@RequestMapping("/api/Base/Usuarios")
+@RequestMapping("/api/Usuarios")
 public class UsuarioController {
 
     @Autowired
@@ -30,6 +33,11 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarios);
     } 
 
+    @PostMapping
+    public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario){
+        Usuario nuevouUsuario = usuarioService.save(usuario);
+        return ResponseEntity.ok(nuevouUsuario);  
+    }
     
 
 }
