e670992de403ed702ba7db384ea467087bf20817
Markduoc <marc.corrales@duocuc.cl>
2025-05-26 17:20:55 -0400

    Se añaden dependencias para env

--- src/main/java/com/EcoMarket/Project/ProjectApplication.java
+++ src/main/java/com/EcoMarket/Project/ProjectApplication.java
@@ -3,10 +3,15 @@ package com.EcoMarket.Project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
